8862abd7c13ab4da39a5e0681f26a527213d8f28
niranda <niranda.perera@gmail.com>
2021-12-06 17:11:13 -0500

    changes

--- cylon/cylon_run.py
+++ cylon/cylon_run.py
@@ -6,7 +6,7 @@ import argparse
 from time import sleep
 
 parser = argparse.ArgumentParser(description='generate random data')
-parser.add_argument('-s', dest='script', type=str, help='script', required=True)
+parser.add_argument('-s', dest='script', type=str, nargs='+', help='scripts', required=True)
 parser.add_argument('-r', dest='rows', type=int, nargs='+', help='row cases in millions',
                     required=True)
 parser.add_argument('-w', dest='world', type=int, nargs='+', help='world sizes',
@@ -21,7 +21,6 @@ args = vars(args)
 rows = [int(ii * 1000000) for ii in args['rows']]
 it = args['it']
 world = args['world']
-script = f"{os.getcwd()}/{args['script']}"
 
 print("args: ", args, flush=True)
 
@@ -29,16 +28,20 @@ PYTHON_EXEC = "~/.conda/envs/cylon_dev/bin/python"
 
 TOTAL_NODES = 15
 
+out_dir = f"{os.getcwd()}/results"
+os.makedirs(out_dir, exist_ok=True)
 
-for r in rows:
-    for w in world:
-        print(f"rows {r} world_size {w} starting!", flush=True)           
-        hostfile = "" if w == 1 else "--hostfile nodes.txt"
-        join_exec = f"mpirun --map-by node --report-bindings -mca btl vader,tcp,openib," \
-                    f"self -mca btl_tcp_if_include enp175s0f0 --mca btl_openib_allow_ib 1 " \
-                    f"{hostfile} --bind-to core --bind-to socket -np {w} " \
-                    f"{PYTHON_EXEC} {script} -r {r} -w {w} -i {it} {args['sargs']}"
-        print("running", join_exec, flush=True)
-                              
-        print(f"rows {r} world_size {w} done!\n-----------------------------------------", flush=True)
-    print(f"rows {r} done!\n ====================================== \n", flush=True)
+for s in args['script']:
+    script = f"{os.getcwd()}/{s}"
+    for r in rows:
+        for w in world:
+            print(f"{s} rows {r} world_size {w} starting!", flush=True)           
+            hostfile = "" if w == 1 else "--hostfile nodes.txt"
+            join_exec = f"mpirun --map-by node --report-bindings -mca btl vader,tcp,openib," \
+                        f"self -mca btl_tcp_if_include enp175s0f0 --mca btl_openib_allow_ib 1 " \
+                        f"{hostfile} --bind-to core --bind-to socket -np {w} " \
+                        f"{PYTHON_EXEC} {script} -r {r} -w {w} -i {it} -o {out_dir} {args['sargs']}"
+            print("running", join_exec, flush=True)
+                                
+            print(f"{s} rows {r} world_size {w} done!", flush=True)
+    print(f"{s} done!\n ====================================== \n", flush=True)
--- cylon/scalar-agg.py
+++ cylon/scalar-agg.py
@@ -53,6 +53,6 @@ try:
         gc.collect()
 finally:
     if rank == 0:
-        pd.DataFrame(timing).to_csv(f"{args['out']}/{script}-{w}.csv", index=False)
+        pd.DataFrame(timing).to_csv(f"{args['out']}/{script}_{args['rows']}_{w}.csv", index=False)
     env.finalize()
 
