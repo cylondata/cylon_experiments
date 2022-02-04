93ceaadc68812761005151ed949d6af5d7692186
niranda perera <niranda.perera@gmail.com>
2022-02-03 20:51:31 -0500

    pandas

---
+++ pandas/join.py
@@ -0,0 +1,60 @@
+import os
+import time
+import argparse
+import math
+import os 
+import gc
+import numpy as np
+import pandas as pd
+
+from numpy.random import default_rng
+
+
+parser = argparse.ArgumentParser(description='generate random data')
+
+parser.add_argument('-r', dest='rows', type=int, help='number of rows', required=True, nargs='+')
+parser.add_argument('-i', dest='it', type=int, help='iterations', default=1)
+parser.add_argument('-u', dest='unique', type=float, default=1.0, help="unique factor")
+
+args = parser.parse_args()
+args = vars(args)
+print(args, flush=True)
+
+rows = args['rows']
+it = args['it']
+script = os.path.basename(__file__).replace('.py', '')
+
+
+if __name__ == "__main__":
+    for r in rows:
+        max_val = r * args['unique']
+        rng = default_rng()
+        frame_data = rng.integers(0, max_val, size=(r, 2)) 
+        frame_data1 = rng.integers(0, max_val, size=(r, 2)) 
+        print(f"data generated", flush=True)
+        
+        timing = {'rows': [], 'world':[], 'it':[], 'time':[]}
+
+        try:           
+            for i in range(it):
+                df_l = pd.DataFrame(frame_data, columns=["col0", "col1"])
+                df_r = pd.DataFrame(frame_data1, columns=["col0", "col1"])
+                print(f"data loaded", flush=True)
+
+
+                t1 = time.time()
+                out = df_l.merge(df_r, on='col0', how='inner', suffixes=('_left', '_right'))
+                t2 = time.time()
+
+                timing['rows'].append(r)
+                timing['world'].append(1)
+                timing['it'].append(i)
+                timing['time'].append((t2 - t1) * 1000)
+                print(f"timings {r} 1 {i} {(t2 - t1) * 1000:.0f} ms, {out.shape[0]}", flush=True)
+                
+                del df_l 
+                del df_r
+                del out 
+                gc.collect()
+        finally:
+            pd.DataFrame(timing).to_csv(f'{script}.csv', mode='a', index=False, header=False)
